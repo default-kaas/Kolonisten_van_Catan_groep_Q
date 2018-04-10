62c40ae9ce6fe6b1151304d986499a50e4ff466d
Rub3nW3st <Rubenwestermann12@gmail.com>
2018-04-10 15:54:05 +0200

    Still test
    Test xD

--- Project_Kolonisten_Van_Catan/src/MainTestClass.java
+++ Project_Kolonisten_Van_Catan/src/MainTestClass.java
@@ -4,6 +4,7 @@ public class MainTestClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test");
+		System.out.println("Still test");
 	}
 
 }

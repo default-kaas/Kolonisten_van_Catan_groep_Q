7fb99fad1c253d2490c2f0b7f6d443ed599d366e
Rub3nW3st <Rubenwestermann12@gmail.com>
2018-04-11 14:18:41 +0200

    Packages + Main
    Alle Packages gemaakt.

--- Project_Kolonisten_Van_Catan/src/View/MainClass.java
+++ Project_Kolonisten_Van_Catan/src/View/MainClass.java
@@ -1,8 +1,10 @@
+package View;
 
-public class MainTestClass {
+public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Main Class!");
 	}
 
 }

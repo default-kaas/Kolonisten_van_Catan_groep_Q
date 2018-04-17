dab1f4ff8f05196f6c0592f36754b3c65461d763
Danish Afzal <37477025+Ryokazo@users.noreply.github.com>
2018-04-17 21:20:03 +0200

    Dobbelsteen
    dobbelsteen panel aangepast. Afbeelding toegevoegd

--- Project_Kolonisten_Van_Catan/src/Controller/Dobbelsteen.java
+++ Project_Kolonisten_Van_Catan/src/Controller/Dobbelsteen.java
@@ -5,31 +5,35 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Dobbelsteen {
 
-Random random = new Random();
+	Random random = new Random();
 
-private int min = 1;
-private int max = 6;
-private int value1;
-private int value2;
+	private int min = 1;
+	private int max = 6;
+	private int value1;
+	private int value2;
 
-public Dobbelsteen() {
-	dobbelen();
-}
+	public Dobbelsteen() {
+		dobbelen();
+	}
 
+	public int dobbelen() {
 
-public int dobbelen() {
-	
-	int sum = 0;
-	
-	//Each dice gives a random number between 1 and 6. Both are added up to make up for the sum which is then returned.
-	value1 = random.nextInt(max - min + 1) + min;
-	value2 = random.nextInt(max - min + 1) + min;
-	
-	sum = value1 + value2;
-	
-	return sum;
-	
-	
-	
-}
+		int sum = 0;
+
+		// Each dice gives a random number between 1 and 6. Both are added up to make up
+		// for the sum which is then returned.
+		value1 = random.nextInt(max - min + 1) + min;
+		value2 = random.nextInt(max - min + 1) + min;
+
+		sum = value1 + value2;
+
+		return sum;
+
+	}
+
+	public boolean thrown() {
+		//This method checks if a player has thrown already thrown the dice once.
+		
+		return false;
+	}
 }
--- Project_Kolonisten_Van_Catan/src/View/DobbelsteenPanel.java
+++ Project_Kolonisten_Van_Catan/src/View/DobbelsteenPanel.java
@@ -4,15 +4,25 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class DobbelsteenPanel extends JPanel {
+	private ImageIcon dice;	
+	private JLabel label1;
 	public DobbelsteenPanel() {
 
+		dice = new ImageIcon(getClass().getResource("/images/dice.png"));
+		label1 = new JLabel(dice);
+		
 		// Dit is ff ORANGE voor een test, mag je weghalen
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		int Height = (int) (screenSize.getHeight() * 0.19);
+		this.add(label1);
 		this.setPreferredSize(new Dimension(256, Height));
 		this.setBackground(Color.ORANGE);
+		
+		
 	}
 }

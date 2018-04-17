d2719bfd8453459d8c52a8af75d41dbd63a22ad0
Danish Afzal <d.afzal@hotmail.com>
2018-04-17 20:14:29 +0200

    Dice changes
    -Variables waardes op de juiste plek gezet om de dobbelsteen correct te laten functioneren.
    -Commentaar toegevoegd.

--- Project_Kolonisten_Van_Catan/src/Controller/Dobbelsteen.java
+++ Project_Kolonisten_Van_Catan/src/Controller/Dobbelsteen.java
@@ -9,8 +9,8 @@ Random random = new Random();
 
 private int min = 1;
 private int max = 6;
-private int value1 = random.nextInt(max - min + 1) + min;
-private int value2 = random.nextInt(max - min + 1) + min;
+private int value1;
+private int value2;
 
 public Dobbelsteen() {
 	dobbelen();
@@ -21,8 +21,11 @@ public int dobbelen() {
 	
 	int sum = 0;
 	
-	sum += value1 + value2;
+	//Each dice gives a random number between 1 and 6. Both are added up to make up for the sum which is then returned.
+	value1 = random.nextInt(max - min + 1) + min;
+	value2 = random.nextInt(max - min + 1) + min;
 	
+	sum = value1 + value2;
 	
 	return sum;
 	

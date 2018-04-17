a04e7f89a5e8d4c262f78466636c6cba792da107
Rub3nW3st <37476580+Rub3nW3st@users.noreply.github.com>
2018-04-17 16:41:49 +0200

    Layout

--- Project_Kolonisten_Van_Catan/src/View/GUI.java
+++ Project_Kolonisten_Van_Catan/src/View/GUI.java
@@ -4,15 +4,19 @@ import javax.swing.JFrame;
 
 public class GUI extends JFrame {
 	Spelscherm Spelscherm;
+
 	public GUI() {
 		Spelscherm = new Spelscherm();
 		setResizable(false);
 		this.setContentPane(Spelscherm);
+		setExtendedState(JFrame.MAXIMIZED_BOTH);
+		this.setUndecorated(true);
 		this.setTitle("Project Kolonisten van Catan, Groep Q");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
 		this.pack();
 		this.setLocationRelativeTo(null);
+		
 
 	}
 

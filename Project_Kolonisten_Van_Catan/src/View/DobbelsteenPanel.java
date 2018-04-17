a3cd52167d370ffd49ee431c7b940552801741da
Rub3nW3st <Rubenwestermann12@gmail.com>
2018-04-17 22:09:34 +0200

    Revert "Dice toegevoegt"
    This reverts commit fa5df87d631b945316537adaa25ba5c067a4c875.

--- Project_Kolonisten_Van_Catan/src/View/DobbelsteenPanel.java
+++ Project_Kolonisten_Van_Catan/src/View/DobbelsteenPanel.java
@@ -2,46 +2,28 @@ package View;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Graphics;
 import java.awt.Toolkit;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
-import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class DobbelsteenPanel extends JPanel {
-	private ImageIcon dice;
+	private ImageIcon dice;	
 	private JLabel label1;
-
-	BufferedImage image;
-
-
 	public DobbelsteenPanel() {
-		try {
-			image = ImageIO.read(new File("images/dice.png"));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
+		dice = new ImageIcon("/Project_Kolonisten_Van_Catan/images/dice.png");
+		label1 = new JLabel(dice);
+		label1.setSize(this.getWidth(), getHeight());
+		
 		// Dit is ff ORANGE voor een test, mag je weghalen
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		int Height = (int) (screenSize.getHeight() * 0.19);
-//		this.add(label1);
+		this.add(label1);
 		this.setPreferredSize(new Dimension(256, Height));
 		this.setBackground(Color.WHITE);
-
+		
+		
 	}
-
-	@Override
-	protected void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-		int Height = (int) (screenSize.getHeight() * 0.19);
-		g.drawImage(image, 0, 0, 128 ,128, this);
-	}
-
 }

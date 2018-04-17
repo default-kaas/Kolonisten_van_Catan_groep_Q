1be8f384eb345f2da730b0ded7d6ec3779e753c2
Rub3nW3st <37476580+Rub3nW3st@users.noreply.github.com>
2018-04-17 09:54:44 +0200

    DatabaseConnect toegevoegt
    Nog niet helemaal af, maar dit is al geconnect met onze database van vorige blok. Nu kunnen we tenminste beginnen.

--- Project_Kolonisten_Van_Catan/src/DatabaseConnect/DatabaseConnect.java
+++ Project_Kolonisten_Van_Catan/src/DatabaseConnect/DatabaseConnect.java
@@ -1,5 +1,63 @@
 package DatabaseConnect;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class DatabaseConnect {
+	Connection m_Conn;
+
+	public DatabaseConnect() throws SQLException {
+		m_Conn = null;
+		if ((loadDataBaseDriver("com.mysql.jdbc.Driver")) && (makeConnection())) {
+			doSomeQuerying();
+
+			// TODO Auto-generated catch block
+
+		}
+	}
+
+	public boolean loadDataBaseDriver(String driverName) {
+
+		try {
+			Class.forName(driverName);
+			return true;
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e);
+
+			return false;
+		}
+	}
+
+	public boolean makeConnection() {
+		// TODO Auto-generated method stub
+
+		String url = "jdbc:mysql://databases.aii.avans.nl:3306/rswester_db2";
+		String user = "rswester";
+		String pwd = "Ab12345";
+		try {
+			m_Conn = DriverManager.getConnection(url, user, pwd);
+
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			return false;
+		}
+
+	}
+
+	public void doSomeQuerying() throws SQLException {
+		// TODO Auto-generated method stub
+		Statement stmt = m_Conn.createStatement();
+		ResultSet rs = stmt.executeQuery("select gebruikersnaam from gebruiker");
+
+		while (rs.next()) {
+			System.out.println(rs.getString("gebruikersnaam"));
+
+		}
+	}
 
 }
--- Project_Kolonisten_Van_Catan/src/View/MainClass.java
+++ Project_Kolonisten_Van_Catan/src/View/MainClass.java
@@ -1,10 +1,20 @@
 package View;
 
+import java.sql.SQLException;
+
+import DatabaseConnect.DatabaseConnect;
+
 public class MainClass {
 
 	public static void main(String[] args) {
 
 		GUI Spel = new GUI();
+		try {
+			DatabaseConnect Test = new DatabaseConnect();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }

381a3e5752869b347cca5371345ed720ac682a20
Rub3nW3st <Rubenwestermann12@gmail.com>
2018-04-18 14:47:53 +0200

    close button added
    Voor nu zit er een close button in SpelerInfo panel, die verander ik later nog!

--- Project_Kolonisten_Van_Catan/src/View/SpelerInformatiePanel.java
+++ Project_Kolonisten_Van_Catan/src/View/SpelerInformatiePanel.java
@@ -3,15 +3,21 @@ package View;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 import Model.Player;
 
-public class SpelerInformatiePanel extends JPanel{
+public class SpelerInformatiePanel extends JPanel implements ActionListener{
 	private Player speler1 = new Player();
+	private JButton close;
 	public SpelerInformatiePanel() {
-		
+		close = new JButton("Close game!");
+		this.add(close);
+		close.addActionListener(this);
 		//Dit is ff red voor een test, mag je weghalen
 		//this.setPreferredSize(new Dimension(300,475));
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -19,5 +25,9 @@ public class SpelerInformatiePanel extends JPanel{
 		int width = (int) ((screenSize.getWidth() - size)/2);
 		this.setPreferredSize(new Dimension(width, (int)(screenSize.getHeight()*0.49)));
 		this.setBackground(Color.RED);
+	}
+	public void actionPerformed(ActionEvent arg0) {
+		// Removes button when a player has thrown a dice to force one throw per round.
+		System.exit(0);
 	}
 }

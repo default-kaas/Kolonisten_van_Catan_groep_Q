c98819b420defb630b27bd7379635a8363b27095
ThijsReba <mwreiche@avans.nl>
2018-04-18 18:03:59 +0200

    log in panel
    werkelijk panel

--- Project_Kolonisten_Van_Catan/src/View/LogInPanel.java
+++ Project_Kolonisten_Van_Catan/src/View/LogInPanel.java
@@ -2,17 +2,52 @@ package View;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
-public class LogInPanel extends JPanel {
-
+public class LogInPanel extends JPanel implements ActionListener{
+	JLabel username, password;
+	JTextField textField;
+	JButton button;
+	JPasswordField passwordField;
+	
 	public LogInPanel() {
+		username = new JLabel("Username");
+		password = new JLabel("Password");
+		textField = new JTextField();
+		passwordField = new JPasswordField();
+		button = new JButton("Login");
+		button.addActionListener(this);
+		
+		username.setBounds(80, 70, 200, 30);
+		password.setBounds(80, 110, 200, 30);
+		textField.setBounds(300, 70, 200, 30);
+		passwordField.setBounds(300, 110, 200, 30);
+		button.setBounds(150, 160, 100, 30);
+		
 		this.setPreferredSize(new Dimension(getMaximumSize()));
 		this.setBackground(Color.blue);
 		signIn();
 	}
-
+	
+	public void actionPerformed(ActionEvent ae) {
+		String uname = textField.getText();
+		String pass = passwordField.getText();
+		
+		if (uname.equals("mw") && pass.equals("abc")) {
+			JLabel label = new JLabel("Welcome:" + uname);
+		} else {
+			JOptionPane.showMessageDialog(this, "Incorrect login or password", "Error", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+	
 	public boolean signIn() {
 		System.out.println("you are signed in");
 		return true;

1da802a00849100607b3730f40e3d6418e841b48
Danish Afzal <d.afzal@hotmail.com>
2018-04-18 20:47:40 +0200

    ChatPanel aanpassingen
    Chatfield toegevoegd met actionlistener.

--- Project_Kolonisten_Van_Catan/src/Controller/ChatPanelController.java
+++ Project_Kolonisten_Van_Catan/src/Controller/ChatPanelController.java
@@ -1,5 +1,15 @@
 package Controller;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ChatPanelController {
+
+	public ArrayList<String> chatArray;
+
+	public ChatPanelController() {
+		chatArray = new ArrayList<String>();
+		
+	}
 
 }

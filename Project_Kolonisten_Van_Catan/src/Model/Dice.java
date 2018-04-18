a8e9d96036672f8ed3389c90637e9dc1a9d8d08f
default-kaas <dogen1996@live.nl>
2018-04-18 20:50:15 +0200

    set en getters dice

--- Project_Kolonisten_Van_Catan/src/Model/Dice.java
+++ Project_Kolonisten_Van_Catan/src/Model/Dice.java
@@ -1,5 +1,28 @@
 package Model;
 
 public class Dice {
-
+	private int value1;
+	private int value2;
+	private boolean canThrow;
+	
+	
+	public int getValue1() {
+		return value1;
+	}
+	public int getValue2() {
+		return value2;
+	}
+	public boolean isCanThrow() {
+		return canThrow;
+	}
+	
+	public void setValue1(int value1) {
+		this.value1 = value1;
+	}
+	public void setValue2(int value2) {
+		this.value2 = value2;
+	}
+	public void setCanThrow(boolean canThrow) {
+		this.canThrow = canThrow;
+	}
 }

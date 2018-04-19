aeb3aa6e7b8821d6c8723bdf2e73d63a26e9fcb7
Danish Afzal <d.afzal@hotmail.com>
2018-04-19 16:01:13 +0200

    Bouwpanel titles toegevoegd
    Zelfde push als net maar opnieuw moeten pushen wegens miscommunicatie

--- Project_Kolonisten_Van_Catan/src/View/BouwPanel.java
+++ Project_Kolonisten_Van_Catan/src/View/BouwPanel.java
@@ -33,6 +33,28 @@ public class BouwPanel extends JPanel {
 		title.setTitleFont(new Font("Arial", Font.BOLD, 20));
 		this.setLayout(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
+		
+		
+		JLabel type = new JLabel("Soort");
+		type.setFont(new Font("Arial", Font.BOLD, 20));
+		c.anchor = GridBagConstraints.WEST;
+		c.gridx = 0;
+		c.gridy = 1;
+		this.add(type, c);
+		
+		JLabel price = new JLabel("Kosten");
+		price.setFont(new Font("Arial", Font.BOLD, 20));
+		c.anchor = GridBagConstraints.CENTER;
+		c.gridx = 4;
+		c.gridy = 1;
+		this.add(price, c);
+		
+		JLabel buy = new JLabel("Kopen");
+		buy.setFont(new Font("Arial", Font.BOLD, 20));
+		c.anchor = GridBagConstraints.EAST;
+		c.gridx = 7;
+		c.gridy = 1;
+		this.add(buy, c);
 
 		// street images
 		ImageIcon road = new ImageIcon("images/Buy/road.png");
@@ -40,27 +62,27 @@ public class BouwPanel extends JPanel {
 		JLabel roadLabel = new JLabel(road);
 		c.anchor = GridBagConstraints.WEST;
 		c.gridx = 0;
-		c.gridy = 1;
+		c.gridy = 2;
 		this.add(roadLabel, c);
 
 		ImageIcon stone = new ImageIcon("images/Resources/stone1.png");
 		JLabel roadStone = new JLabel(stone);
 		c.anchor = GridBagConstraints.CENTER;
 		c.gridx = 3;
-		c.gridy = 1;
+		c.gridy = 2;
 		this.add(roadStone, c);
 
 		ImageIcon wood = new ImageIcon("images/Resources/wood1.png");
 		JLabel roadWood = new JLabel(wood);
 		c.anchor = GridBagConstraints.CENTER;
 		c.gridx = 4;
-		c.gridy = 1;
+		c.gridy = 2;
 		this.add(roadWood, c);
 
 		JButton buyRoad = new JButton("Buy Road");
 		c.anchor = GridBagConstraints.EAST;
 		c.gridx = 7;
-		c.gridy = 1;
+		c.gridy = 2;
 		this.add(buyRoad, c);
 
 		// house images
@@ -69,39 +91,39 @@ public class BouwPanel extends JPanel {
 		JLabel houseLabel = new JLabel(house);
 		c.anchor = GridBagConstraints.WEST;
 		c.gridx = 0;
-		c.gridy = 2;
+		c.gridy = 3;
 		this.add(houseLabel, c);
 
 		JLabel houseStone = new JLabel(stone);
 		c.anchor = GridBagConstraints.CENTER;
 		c.gridx = 2;
-		c.gridy = 2;
+		c.gridy = 3;
 		this.add(houseStone, c);
 
 		JLabel houseWood = new JLabel(wood);
 		c.anchor = GridBagConstraints.CENTER;
 		c.gridx = 3;
-		c.gridy = 2;
+		c.gridy = 3;
 		this.add(houseWood, c);
 
 		ImageIcon wheat = new ImageIcon("images/Resources/wheat1.png");
 		JLabel houseWheat = new JLabel(wheat);
 		c.anchor = GridBagConstraints.CENTER;
 		c.gridx = 4;
-		c.gridy = 2;
+		c.gridy = 3;
 		this.add(houseWheat, c);
 
 		ImageIcon wool1 = new ImageIcon("images/Resources/sheep1.png");
 		JLabel houseWool = new JLabel(wool1);
 		c.anchor = GridBagConstraints.CENTER;
 		c.gridx = 5;
-		c.gridy = 2;
+		c.gridy = 3;
 		this.add(houseWool, c);
 
 		JButton buyHouse = new JButton("Buy House");
 		c.anchor = GridBagConstraints.EAST;
 		c.gridx = 7;
-		c.gridy = 2;
+		c.gridy = 3;
 		this.add(buyHouse, c);
 
 		// city images
@@ -111,7 +133,7 @@ public class BouwPanel extends JPanel {
 		JLabel cityLabel = new JLabel(city);
 		c.anchor = GridBagConstraints.WEST;
 		c.gridx = 0;
-		c.gridy = 3;
+		c.gridy = 4;
 		this.add(cityLabel, c);
 
 		ImageIcon ore = new ImageIcon("images/Resources/ore1.png");
@@ -119,37 +141,37 @@ public class BouwPanel extends JPanel {
 		JLabel cityOre1 = new JLabel(ore);
 		c.anchor = GridBagConstraints.CENTER;
 		c.gridx = 2;
-		c.gridy = 3;
+		c.gridy = 4;
 		this.add(cityOre1, c);
 
 		JLabel cityOre2 = new JLabel(ore);
 		c.anchor = GridBagConstraints.CENTER;
 		c.gridx = 3;
-		c.gridy = 3;
+		c.gridy = 4;
 		this.add(cityOre2, c);
 
 		JLabel cityOre3 = new JLabel(ore);
 		c.anchor = GridBagConstraints.CENTER;
 		c.gridx = 4;
-		c.gridy = 3;
+		c.gridy = 4;
 		this.add(cityOre3, c);
 
 		JLabel cityWheat1 = new JLabel(wheat);
 		c.anchor = GridBagConstraints.CENTER;
 		c.gridx = 5;
-		c.gridy = 3;
+		c.gridy = 4;
 		this.add(cityWheat1, c);
 
 		JLabel cityWheat2 = new JLabel(wheat);
 		c.anchor = GridBagConstraints.CENTER;
 		c.gridx = 6;
-		c.gridy = 3;
+		c.gridy = 4;
 		this.add(cityWheat2, c);
 
 		JButton buyCity = new JButton("Buy City");
 		c.anchor = GridBagConstraints.EAST;
 		c.gridx = 7;
-		c.gridy = 3;
+		c.gridy = 4;
 		this.add(buyCity, c);
 
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();

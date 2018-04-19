94376c8999c2eb582d9da80e564aa69293a4230b
Danish Afzal <d.afzal@hotmail.com>
2018-04-19 20:23:37 +0200

    BouwPanelController
    In de BouwPanelController methods aangemaakt die controleren of de speler genoeg resources heeft om te bouwen.

--- Project_Kolonisten_Van_Catan/src/Controller/BouwPanelController.java
+++ Project_Kolonisten_Van_Catan/src/Controller/BouwPanelController.java
@@ -1,5 +1,67 @@
 package Controller;
 
+import Model.Game;
+import Model.Player;
+
 public class BouwPanelController {
+
+	private Game game;
+
+	public BouwPanelController(Game game) {
+		this.game = game;
+	}
+
+	public boolean checkRecoursesRoad() {
+
+		int wood = game.getMe().getWoodAmount();
+		int stone = game.getMe().getStoneAmount();
+
+		if (wood >= 1 && stone >= 1) {
+
+			return true;
+		} else {
+			return false;
+
+		}
+
+	}
+
+	public boolean checkResourcesHouse() {
+		int wood = game.getMe().getWoodAmount();
+		int stone = game.getMe().getStoneAmount();
+		int wheat = game.getMe().getWheatAmount();
+		int wool = game.getMe().getWoolAmount();
+
+		if (wood >= 1 && stone >= 1 && wool >= 1 && wheat >= 1 && wool >= 1) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	public boolean checkResourcesCity() {
+		int erts = game.getMe().getOreAmount();
+		int graan = game.getMe().getWheatAmount();
+
+		if (erts >= 3 && graan >= 2) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	public boolean checkResourcesDevCard() {
+		int erts = game.getMe().getOreAmount();
+		int graan = game.getMe().getWheatAmount();
+		int wool = game.getMe().getWoolAmount();
+
+		if (erts >= 1 && graan >= 1 && wool >= 1) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 }

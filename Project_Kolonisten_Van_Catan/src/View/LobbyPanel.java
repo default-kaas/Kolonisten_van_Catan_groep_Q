a427d53f5a3c06729a6468a78b898575d20218e4
Masud <masudd@live.nl>
2018-04-19 12:21:33 +0200

    LobbyPanel code toegevoegd
    View code lobbyPanel

--- Project_Kolonisten_Van_Catan/src/View/LobbyPanel.java
+++ Project_Kolonisten_Van_Catan/src/View/LobbyPanel.java
@@ -1,5 +1,79 @@
 package View;
 
-public class LobbyPanel {
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+
+public class LobbyPanel extends JPanel {
+	private JTable playerList;
+	private JTable playerFinding;
+
+	/**
+	 * Create the panel.
+	 */
+	public LobbyPanel() {
+		setPreferredSize(new Dimension(350, 300));
+		setBackground(new Color(157, 24, 31));
+		String[] columns = new String[] { "Id", "Name" };
+		// values
+		Object[][] data = new Object[][] { { 1, "John" }, { 2, "Rambo" }, { 3, "Zorro", 60.0, true }, };
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[] { 186, 164, 0 };
+		gridBagLayout.rowHeights = new int[] { 22, 48, 25, 0, 0, 0, 58, 0 };
+		gridBagLayout.columnWeights = new double[] { 0.0, 0.0, Double.MIN_VALUE };
+		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE };
+		setLayout(gridBagLayout);
+
+		JLabel lblLobby = new JLabel("Lobby");
+		lblLobby.setFont(new Font("Arial", Font.BOLD, 18));
+		GridBagConstraints gbc_lblLobby = new GridBagConstraints();
+		gbc_lblLobby.insets = new Insets(0, 0, 5, 5);
+		gbc_lblLobby.gridx = 0;
+		gbc_lblLobby.gridy = 0;
+		add(lblLobby, gbc_lblLobby);
+		playerList = new JTable(data, columns);
+		GridBagConstraints gbc_table = new GridBagConstraints();
+		gbc_table.fill = GridBagConstraints.HORIZONTAL;
+		gbc_table.insets = new Insets(0, 0, 5, 5);
+		gbc_table.gridwidth = 3;
+		gbc_table.gridx = 0;
+		gbc_table.gridy = 1;
+		add(playerList, gbc_table);
+
+		JButton inviteBtn = new JButton("Invite");
+		GridBagConstraints gbc_inviteBtn = new GridBagConstraints();
+		gbc_inviteBtn.fill = GridBagConstraints.CENTER;
+		gbc_inviteBtn.insets = new Insets(0, 0, 5, 0);
+		gbc_inviteBtn.anchor = GridBagConstraints.SOUTH;
+		gbc_inviteBtn.gridx = 1;
+		gbc_inviteBtn.gridy = 2;
+		add(inviteBtn, gbc_inviteBtn);
+
+		JLabel lblFinding = new JLabel("Finding Players");
+		lblFinding.setFont(new Font("Arial", Font.BOLD, 18));
+		GridBagConstraints gbc_lblFinding = new GridBagConstraints();
+		gbc_lblFinding.insets = new Insets(0, 0, 5, 5);
+		gbc_lblFinding.gridx = 0;
+		gbc_lblFinding.gridy = 4;
+		add(lblFinding, gbc_lblFinding);
+
+		playerFinding = new JTable(data, columns);
+		GridBagConstraints gbc_table_1 = new GridBagConstraints();
+		gbc_table_1.gridwidth = 3;
+		gbc_table_1.insets = new Insets(0, 0, 5, 4);
+		gbc_table_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_table_1.gridx = 0;
+		gbc_table_1.gridy = 5;
+		add(playerFinding, gbc_table_1);
+
+	}
 
 }

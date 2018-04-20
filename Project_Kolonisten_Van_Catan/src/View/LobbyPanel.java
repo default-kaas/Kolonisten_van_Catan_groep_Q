31b1682c67aded2e9b70612354e2246cd540bc67
Danish Afzal <d.afzal@hotmail.com>
2018-04-20 22:12:20 +0200

    LobbyPanel GUI verbetert

--- Project_Kolonisten_Van_Catan/src/View/LobbyPanel.java
+++ Project_Kolonisten_Van_Catan/src/View/LobbyPanel.java
@@ -35,7 +35,7 @@ public class LobbyPanel extends JPanel {
 		JLabel lblLobby = new JLabel("Lobby");
 		lblLobby.setFont(new Font("Arial", Font.BOLD, 100));
 		GridBagConstraints gbc_lblLobby = new GridBagConstraints();
-		gbc_lblLobby.insets = new Insets(0, 0, 0, 500);
+		gbc_lblLobby.insets = new Insets(0, 0, 0, 1000);
 		gbc_lblLobby.anchor = GridBagConstraints.WEST;
 		gbc_lblLobby.gridx = 0;
 		gbc_lblLobby.gridy = 1;
@@ -52,40 +52,77 @@ public class LobbyPanel extends JPanel {
 		}
 		
 		GridBagConstraints gbc_table = new GridBagConstraints();
-		/*gbc_table.fill = GridBagConstraints.HORIZONTAL;*/
 		gbc_table.anchor = GridBagConstraints.WEST;
-		gbc_table.insets = new Insets(0, 0, 0, 500);
+		gbc_table.insets = new Insets(0, 0, 0, 1000);
 		gbc_table.gridwidth = 3;
 		gbc_table.gridx = 0;
 		gbc_table.gridy = 2;
 		add(playerList, gbc_table);
+		
+		
 
-	/*	JButton inviteBtn = new JButton("Invite");
+		JButton refreshBtn = new JButton("Refresh");	
+		refreshBtn.setPreferredSize(new Dimension(200, 100));
+		refreshBtn.setBackground(new Color(59, 89, 182));
+		refreshBtn.setForeground(Color.BLACK);
+		refreshBtn.setFont(new Font("Calibri", Font.BOLD, 30));
 		GridBagConstraints gbc_inviteBtn = new GridBagConstraints();
-		gbc_inviteBtn.insets = new Insets(0, 0, 5, 0);
+		gbc_inviteBtn.insets = new Insets(0,150, 0, 1000);
+		
 		gbc_inviteBtn.anchor = GridBagConstraints.CENTER;
-		gbc_inviteBtn.gridx = 1;
-		gbc_inviteBtn.gridy =0;
-		add(inviteBtn, gbc_inviteBtn);
+		gbc_inviteBtn.gridx = 0;
+		gbc_inviteBtn.gridy = 3;
+		add(refreshBtn, gbc_inviteBtn);
+		
+		//finding
 
-		JLabel lblFinding = new JLabel("Finding Players");
-		lblFinding.setFont(new Font("Arial", Font.BOLD, 18));
+	JLabel lblFinding = new JLabel("Finding Players");
+		lblFinding.setFont(new Font("Arial", Font.BOLD, 100));
 		GridBagConstraints gbc_lblFinding = new GridBagConstraints();
 		gbc_lblFinding.insets = new Insets(0, 0, 5, 5);
 		gbc_lblLobby.anchor = GridBagConstraints.EAST;
-		gbc_lblFinding.gridx = 2;
-		gbc_lblFinding.gridy = 0;
+		gbc_lblFinding.gridx = 1;
+		gbc_lblFinding.gridy = 1;
 		add(lblFinding, gbc_lblFinding);
 
 		playerFinding = new JTable(data, columns);
+		
+		playerFinding = new JTable(data, columns);
+		playerFinding.setFont(new Font("Calibri", Font.BOLD, 30));
+		playerFinding.setRowHeight(100);
+		TableColumnModel columnModel1 = playerFinding.getColumnModel();
+		playerFinding.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		
+		
+		for (int i = 0; i < columnModel1.getColumnCount(); i++) {
+		playerFinding.getColumnModel().getColumn(i).setPreferredWidth(250);
+		}
+		
 		GridBagConstraints gbc_table_1 = new GridBagConstraints();
 		gbc_table_1.gridwidth = 3;
 		gbc_table_1.insets = new Insets(0, 0, 5, 4);
 		gbc_lblLobby.anchor = GridBagConstraints.EAST;
-		gbc_table_1.gridx = 2;
-		gbc_table_1.gridy = 1;
-		add(playerFinding, gbc_table_1);*/
-
+		gbc_table_1.gridx = 1;
+		gbc_table_1.gridy = 2;
+		add(playerFinding, gbc_table_1);
+		
+		
+		JButton inviteBtn = new JButton("Invite Players");	
+		inviteBtn.setPreferredSize(new Dimension(200, 100));
+		inviteBtn.setBackground(new Color(59, 89, 182));
+		inviteBtn.setForeground(Color.BLACK);
+		inviteBtn.setFont(new Font("Calibri", Font.BOLD, 30));
+		GridBagConstraints gbc_inviteBtn1 = new GridBagConstraints();
+		gbc_inviteBtn1.insets = new Insets(0, 0, 0, 0);
+		gbc_inviteBtn1.anchor = GridBagConstraints.CENTER;
+		gbc_inviteBtn1.gridx = 1;
+		gbc_inviteBtn1.gridy = 3;
+		add(inviteBtn, gbc_inviteBtn1);
+	}
+	
+	
+	public void showLobby() {
+		
 	}
 
 }

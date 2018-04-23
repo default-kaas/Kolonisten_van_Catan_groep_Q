ab78db8f7d1c97be322e0e8a10c94f33148fa3fc
Rub3nW3st <Rubenwestermann12@gmail.com>
2018-04-23 21:04:49 +0200

    ?

--- Project_Kolonisten_Van_Catan/src/Controller/LobbyController.java
+++ Project_Kolonisten_Van_Catan/src/Controller/LobbyController.java
@@ -9,37 +9,37 @@ import View.LobbyPanel;
 
 public class LobbyController {
 
-	
 	private LobbyDAO lobbyDAO;
 	private LobbyPanel lobbyPanel;
 	private String username;
 	private InvitePanel invitePanel;
 	private InviteController inviteController;
-	
+
 	public LobbyController(LobbyFrame lobbyFrame, Connection db, String username) {
 		this.username = username;
 		lobbyDAO = new LobbyDAO(db);
 		lobbyPanel = new LobbyPanel(lobbyFrame, this);
-		
-	/*	inviteController = new InviteController(db);
-		invitePanel = new InvitePanel(inviteController);*/		
+
+		inviteController = new InviteController(db);
+		invitePanel = new InvitePanel(inviteController);
 	}
-	
+
 	public LobbyPanel getLobbyPanel() {
 		return lobbyPanel;
 	}
-	
-	
-	public Object[][] showUsers(){
+
+	public Object[][] showUsers() {
 		return lobbyDAO.getUserList(username);
 	}
-	
-/*	public InvitePanel getInvitePanel() {
+
+	public InvitePanel getInvitePanel() {
 		return invitePanel;
-	}*/
-	public Object[][] showInvites(){
+	}
+
+	public Object[][] showInvites() {
 		return lobbyDAO.getInvitedGames(username);
 	}
+
 	public void respondToInvite(int gameId, boolean Accept) {
 		lobbyDAO.respondToInvite(username, gameId, Accept);
 	}

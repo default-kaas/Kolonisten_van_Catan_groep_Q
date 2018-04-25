69a9469fe7975984d64fcd7a423fb1838beb76a8
Rub3nW3st <Rubenwestermann12@gmail.com>
2018-04-26 00:28:48 +0200

    Je kan nu nieuwe players uitnodigen als ze weigeren :+1:

---
+++ Project_Kolonisten_Van_Catan/src/Model/Invite.java
@@ -0,0 +1,62 @@
+package Model;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+
+import Database.InviteDAO;
+import View.InvitePanel;
+
+public class Invite {
+	private InviteDAO InviteDAO;
+	private String username;
+	private int idspel;
+	private Integer[] volgnrs;
+
+	public Invite(Connection db_conn, int idspel, String username) {
+		this.idspel = idspel;
+		this.username = username;
+		InviteDAO = new InviteDAO(db_conn);
+		volgnrs = new Integer[3];
+	}
+
+	public Object[][] showUsers() {
+		return InviteDAO.getUserList(username, idspel);
+	}
+
+	public Object[][] showInviteUsers() {
+		int i = InviteDAO.getRejectedVolgnr(idspel);
+		if(i == -1) {
+		}
+		else {
+			volgnrs[i-2] = null;
+		}
+	
+		return InviteDAO.getInviteUserList(username);
+	}
+
+	public void invitePlayer(String userName) {
+		
+		int i = 0;
+		while (i < 2) {
+		
+			if (volgnrs[i] == null) {
+				System.out.println(i+2);
+				volgnrs[i] = 2;
+				break;
+			}
+			i++;
+		}
+		i +=2;
+		System.out.println(i);
+		InviteDAO.invitePlayers(userName, idspel, i);
+		
+		
+
+	}
+
+
+	public int getNumberInvited() {
+		// TODO Auto-generated method stub
+		return InviteDAO.getNumberInvited(idspel);
+	}
+}

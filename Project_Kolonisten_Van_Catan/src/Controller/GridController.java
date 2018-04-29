03b5029063fccfd1db625e530aa0b8eeaa61853c
ThijsReba <mwreiche@avans.nl>
2018-04-29 14:17:45 +0200

    little update

--- Project_Kolonisten_Van_Catan/src/Controller/GridController.java
+++ Project_Kolonisten_Van_Catan/src/Controller/GridController.java
@@ -7,10 +7,10 @@ public class GridController {
 	private int x;
 	private int y;
 	private int kind;
-	// private int x1; x locatie eerste hoekpunt
-	// private int y1; y locatie eerste hoekpunt
-	// private int x2; x locatie tweede hoekpunt
-	// private int y2; y locatie tweede hoekpunt
+	private int x1; //x locatie eerste hoekpunt
+	private int y1; //y locatie eerste hoekpunt
+	private int x2; //x locatie tweede hoekpunt
+	private int y2; //y locatie tweede hoekpunt
 
 	public GridController() {
 		gridPanel = new GridPanel();
@@ -23,32 +23,59 @@ public class GridController {
 	 * 
 	 * forloop door lijst coördinaten get value met index i
 	 * 
-	 * ALLE ZIJDE for(int i=0; i<arraysize; i++){ x1 = x locatie eerste hoekpunt y1
-	 * = y locatie eerste hoekpunt x2 = x locatie tweede hoekpunt y2 = y locatie
-	 * tweede hoekpunt private int counter; private int location;
+	 * ALLE ZIJDE
+	 * for(int i=0; i<arraysize; i++){
+	 * x1 = x locatie eerste hoekpunt
+	 * y1 = y locatie eerste hoekpunt
+	   x2 = x locatie tweede hoekpunt
+	   y2 = y locatie tweede hoekpunt
+	   private int counter;
+	   private int location;
 	 * 
 	 * if statement checken of x,y er tussen valt
 	 * 
-	 * whichKind(x1, y1, x2, y2); if(kind == 1){ if(x>(x1-5) && x<(x2+5) && y>(y1-5)
-	 * && y<y2+5){ location = i; counter++; } } else if(kind == 2){ ZWNO niet goed
-	 * if(x>(x1-5) && x<(x2+5) && y>(y1-5) && y<y2+5){ location = i; counter++; } }
-	 * else if(kind == 3){ NWZO niet goed if(x>(x1-5) && x<(x2+5) && y>(y1-5) &&
-	 * y<y2+5){ location = i; counter++; } }
-	 * 
+	 * whichKind(x1, y1, x2, y2);
+	 * if(kind == 1){ 
+		 * if(x>(x1-5) && x<(x2+5) && y>(y1-5) && y<y2+5){
+		 * 	location = i;
+		 * 	counter++;
+		 * }
+	 * } else if(kind == 2){
+	 * 		ZWNO niet goed
+	 * 			if(x>(x1-5) && x<(x2+5) && y>(y1-5) && y<y2+5){
+	 * 				location = i;
+	 * 				counter++;
+	 * 			}
+	 * 	} else if(kind == 3){
+	 * 		NWZO niet goed
+	 * 			if(x>(x1-5) && x<(x2+5) && y>(y1-5) && y<y2+5){
+	 * 				location = i;
+	 * 				counter++;
+	 * 			}
+	 * 		}
 	 * }
 	 * 
-	 * if(counter = 1){ dan heb je de straat }
-	 * 
+	 * if(counter = 1){
+	 * 		dan heb je de straat
+	 * 	}
 	 * 
 	 * DIT IS VOOR DORP
 	 * 
-	 * for(int i=0; i<arraysize; i++){ private int r = 10; private int counter;
-	 * private int location;
+	 * for(int i=0; i<arraysize; i++){
+	 * 		private int r = 10;
+	 * 		private int counter;
+	 *		private int location;
 	 * 
-	 * if statement checken of x,y er tussen valt if((x-x1)*(x-x1) + (y-y1)*(x-y1)<
-	 * r*r || (x-x1)*(x-x1) + (y-y1)*(x-y1) == r*r){ location = i; counter++; } }
+	 * if statement checken of x,y er tussen valt
+	 * if((x-x1)*(x-x1) + (y-y1)*(x-y1)<r*r || (x-x1)*(x-x1) + (y-y1)*(x-y1) == r*r){
+	 * 		location = i;
+	 * 		counter++;
+	 * 		}
+	 * }
 	 * 
-	 * if(counter = 1){ dan heb je het hoekpunt dorp }
+	 * if(counter = 1){ 
+	 * 		dan heb je het hoekpunt dorp
+	 * 		}
 	 * }
 	 */
 

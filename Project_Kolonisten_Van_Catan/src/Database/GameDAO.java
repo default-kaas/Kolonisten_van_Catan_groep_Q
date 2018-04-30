74f6eb055d3006117b8e841e1e63b2e37514e088
Rub3nW3st <Rubenwestermann12@gmail.com>
2018-04-30 22:48:02 +0200

    Resources komen nu uit db, er word alleen nog niet geschreven naar het db

--- Project_Kolonisten_Van_Catan/src/Database/GameDAO.java
+++ Project_Kolonisten_Van_Catan/src/Database/GameDAO.java
@@ -27,4 +27,64 @@ public class GameDAO {
 
 	}
 
+	public Object[][] getResourceAmount(int gameId, String userName, int i2) {
+		Object[][] x = null;
+		try {
+			Statement stmt = m_Conn.createStatement();
+			ResultSet rs;
+			String query = null;
+			switch (i2) {
+			case 0:
+				query = "select sgk.idgrondstofkaart as id, gk.idgrondstofsoort as soort from spelergrondstofkaart as sgk join speler as s on sgk.idspeler = s.idspeler join grondstofkaart as gk on gk.idgrondstofkaart = sgk.idgrondstofkaart   where sgk.idspel = "
+						+ gameId + " and username = '" + userName + "' and gk.idgrondstofsoort = 'W'";
+				break;
+			case 1:
+				query = "select sgk.idgrondstofkaart as id, gk.idgrondstofsoort as soort from spelergrondstofkaart as sgk join speler as s on sgk.idspeler = s.idspeler join grondstofkaart as gk on gk.idgrondstofkaart = sgk.idgrondstofkaart   where sgk.idspel = "
+						+ gameId + " and username = '" + userName + "' and gk.idgrondstofsoort = 'G'";
+				break;
+			case 2:
+				query = "select sgk.idgrondstofkaart as id, gk.idgrondstofsoort as soort from spelergrondstofkaart as sgk join speler as s on sgk.idspeler = s.idspeler join grondstofkaart as gk on gk.idgrondstofkaart = sgk.idgrondstofkaart   where sgk.idspel = "
+						+ gameId + " and username = '" + userName + "' and gk.idgrondstofsoort = 'B'";
+				break;
+			case 3:
+				query = "select sgk.idgrondstofkaart as id, gk.idgrondstofsoort as soort from spelergrondstofkaart as sgk join speler as s on sgk.idspeler = s.idspeler join grondstofkaart as gk on gk.idgrondstofkaart = sgk.idgrondstofkaart   where sgk.idspel = "
+						+ gameId + " and username = '" + userName + "' and gk.idgrondstofsoort = 'E'";
+				break;
+			case 4:
+				query = "select sgk.idgrondstofkaart as id, gk.idgrondstofsoort as soort from spelergrondstofkaart as sgk join speler as s on sgk.idspeler = s.idspeler join grondstofkaart as gk on gk.idgrondstofkaart = sgk.idgrondstofkaart   where sgk.idspel = "
+						+ gameId + " and username = '" + userName + "' and gk.idgrondstofsoort = 'H'";
+				break;
+			}
+
+			rs = stmt.executeQuery(query);
+
+			x = new Object[getRowCount(rs)][2];
+			rs.beforeFirst();
+			int i = 0;
+			while (rs.next()) {
+				x[i][0] = rs.getString("id");
+				String test = rs.getString("soort");
+				char test1 = test.charAt(0);
+				x[i][1] = test1;
+				i++;
+			}
+
+			return x;
+		} catch (SQLException e) {
+			return x;
+		}
+
+	}
+
+	private int getRowCount(ResultSet rs) {
+		try {
+			if (rs != null) {
+				rs.last();
+				return rs.getRow();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return 0;
+	}
 }

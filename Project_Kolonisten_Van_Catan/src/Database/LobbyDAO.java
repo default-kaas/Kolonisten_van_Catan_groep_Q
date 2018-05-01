0e71d2ceed2d40774698c17153aae3e146d70541
Rub3nW3st <Rubenwestermann12@gmail.com>
2018-05-01 19:26:57 +0200

    Game board word nu aangemaakt, word later waarschijnlijk nog verplaatst naar Board controller

--- Project_Kolonisten_Van_Catan/src/Database/LobbyDAO.java
+++ Project_Kolonisten_Van_Catan/src/Database/LobbyDAO.java
@@ -5,10 +5,12 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class LobbyDAO {
 
-	Connection m_Conn;
+	private Connection m_Conn;
+	Random random = new Random();
 
 	public LobbyDAO(Connection db_conn) {
 
@@ -43,13 +45,166 @@ public class LobbyDAO {
 
 			final String QUERY2 = "INSERT INTO spel (idspel, israndomboard, eersteronde) VALUES (" + newGameID + ", "
 					+ RandomBoard + ", 1)";
-			Statement statement2 = m_Conn.createStatement();
-			statement2.executeUpdate(QUERY2);
+			Statement statement = m_Conn.createStatement();
+			statement.executeUpdate(QUERY2);
 
 			final String QUERY3 = "INSERT INTO speler (idspeler, idspel, username, kleur, speelstatus, shouldrefresh, volgnr) VALUES ("
 					+ newSpelerID + ", " + newGameID + ", '" + username + "', \"rood\", \"uitdager\", 0, 1);";
-			Statement statement3 = m_Conn.createStatement();
-			statement3.executeUpdate(QUERY3);
+
+			statement.executeUpdate(QUERY3);
+
+			final String QUERY4 = "INSERT INTO spelergrondstofkaart (idspel, idgrondstofkaart) SELECT idspel, idgrondstofkaart FROM grondstofkaart join spel where idspel = "
+					+ newGameID + ";";
+			statement.executeUpdate(QUERY4);
+
+			int B = 0;
+			int E = 0;
+			int G = 0;
+			int H = 0;
+			int W = 0;
+
+			if (RandomBoard) {
+				System.out.println("RandomBoard");
+			} else {
+				for (int i = 0; i < 19; i++) {
+					System.out.println(i);
+					int randomInt = random.nextInt(5);
+					char ResourceType = 0;
+					Boolean nextResource = false;
+					if (i == 9) {
+						final String QUERY5 = "INSERT INTO tegel (idspel, idtegel, x , y , idgrondstofsoort, idgetalfiche) VALUES ("
+								+ newGameID + ", " + (i + 1) + ",6,6,'X', null);";
+						statement.executeUpdate(QUERY5);
+					} else {
+						while (!nextResource) {
+							if (randomInt == 0 && B < 3) {
+								B++;
+								ResourceType = 'B';
+								nextResource = true;
+							} else if (randomInt == 1 && E < 3) {
+								E++;
+								ResourceType = 'E';
+								nextResource = true;
+							} else if (randomInt == 2 && G < 4) {
+								G++;
+								ResourceType = 'G';
+								nextResource = true;
+							} else if (randomInt == 3 && H < 4) {
+								H++;
+								ResourceType = 'H';
+								nextResource = true;
+							} else if (randomInt == 4 && W < 4) {
+								W++;
+								ResourceType = 'W';
+								nextResource = true;
+							} else {
+								randomInt = random.nextInt(4);
+							}
+						}
+						System.out.println(ResourceType);
+						int[] Position = new int[2];
+						int numberChips = -1;
+						switch (i) {
+						case 0:
+							Position[0] = 2;
+							Position[1] = 4;
+							numberChips = 10;
+							break;
+						case 1:
+							Position[0] = 3;
+							Position[1] = 6;
+							numberChips = 2;
+							break;
+						case 2:
+							Position[0] = 4;
+							Position[1] = 8;
+							numberChips = 8;
+							break;
+						case 3:
+							Position[0] = 3;
+							Position[1] = 3;
+							numberChips = 14;
+							break;
+						case 4:
+							Position[0] = 4;
+							Position[1] = 5;
+							numberChips = 6;
+							break;
+						case 5:
+							Position[0] = 5;
+							Position[1] = 7;
+							numberChips = 4;
+							break;
+						case 6:
+							Position[0] = 6;
+							Position[1] = 9;
+							numberChips = 1;
+							break;
+						case 7:
+							Position[0] = 4;
+							Position[1] = 2;
+							numberChips = 12;
+							break;
+						case 8:
+							Position[0] = 5;
+							Position[1] = 4;
+							numberChips = 9;
+							break;
+						case 10:
+							Position[0] = 7;
+							Position[1] = 8;
+							numberChips = 13;
+							break;
+						case 11:
+							Position[0] = 8;
+							Position[1] = 10;
+							numberChips = 7;
+							break;
+						case 12:
+							Position[0] = 6;
+							Position[1] = 3;
+							numberChips = 18;
+							break;
+						case 13:
+							Position[0] = 7;
+							Position[1] = 5;
+							numberChips = 3;
+							break;
+						case 14:
+							Position[0] = 8;
+							Position[1] = 7;
+							numberChips = 16;
+							break;
+						case 15:
+							Position[0] = 9;
+							Position[1] = 9;
+							numberChips = 15;
+							break;
+						case 16:
+							Position[0] = 8;
+							Position[1] = 4;
+							numberChips = 17;
+							break;
+						case 17:
+							Position[0] = 9;
+							Position[1] = 6;
+							numberChips = 5;
+							break;
+						case 18:
+							Position[0] = 10;
+							Position[1] = 8;
+							numberChips = 11;
+							break;
+						}
+
+						final String QUERY5 = "INSERT INTO tegel (idspel, idtegel, x , y , idgrondstofsoort, idgetalfiche) VALUES ("
+								+ newGameID + ", " + (i + 1) + ", " + Position[0] + "," + Position[1] + ",'"
+								+ ResourceType + "'," + numberChips + ");";
+						statement.executeUpdate(QUERY5);
+					}
+				}
+			}
+
 			return newGameID;
 		} catch (SQLException e) {
 			System.out.println(e);

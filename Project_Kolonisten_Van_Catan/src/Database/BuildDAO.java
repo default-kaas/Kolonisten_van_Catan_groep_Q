fa036eb8a12a323add148c7910f190108d03661a
DanishAfzal <d.afzal@hotmail.com>
2018-05-08 23:58:21 +0200

    Gare push
    Gratis geld

--- Project_Kolonisten_Van_Catan/src/Database/BuildDAO.java
+++ Project_Kolonisten_Van_Catan/src/Database/BuildDAO.java
@@ -11,19 +11,20 @@ public class BuildDAO {
 	public BuildDAO(Connection db_conn) {
 		this.db_conn = db_conn;
 	}
-	
+
 	public int checkIfPlayerHasHouse(int gameId, int playerId) {
 		try {
 			Statement stmt = db_conn.createStatement();
 			ResultSet rs;
-			rs = stmt.executeQuery("select count(spelerstuk.idspeler) AS aantal from spelerstuk join stuk ON spelerstuk.idstuk = stuk.idstuk join speler ON speler.idspeler = spelerstuk.idspeler where stuk.stuksoort = 'dorp' AND speler.idspel = "+gameId+ 
-					" AND spelerstuk.idspeler = "+playerId+" AND spelerstuk.x_van IS NOT null  AND spelerstuk.y_van IS NOT null");
+			rs = stmt.executeQuery(
+					"select count(spelerstuk.idspeler) AS aantal from spelerstuk join stuk ON spelerstuk.idstuk = stuk.idstuk join speler ON speler.idspeler = spelerstuk.idspeler where stuk.stuksoort = 'dorp' AND speler.idspel = "
+							+ gameId + " AND spelerstuk.idspeler = " + playerId
+							+ " AND spelerstuk.x_van IS NOT null  AND spelerstuk.y_van IS NOT null");
 			rs.next();
 			int amount = rs.getInt("aantal");
-			System.out.println(amount);
 			return amount;
 		} catch (SQLException e) {
-			System.out.println(e);
+
 			return 0;
 		}
 	}

b5a7620f41547da7ea2052cee9ed2d05c5478d3f
ThijsReba <mwreiche@avans.nl>
2018-05-09 16:47:48 +0200

    grid aanpassng

--- Project_Kolonisten_Van_Catan/src/Controller/GridController.java
+++ Project_Kolonisten_Van_Catan/src/Controller/GridController.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import Model.Board;
 import Model.Corner;
 import Model.Game;
+import Model.Street;
 import View.GridPanel;
 
 public class GridController {
@@ -20,13 +21,14 @@ public class GridController {
 	private Game gameNr;
 	private Connection db_conn;
 	private Board myBoard;
+	private Street myStreet;
 
 	public GridController() {
 		gridPanel = new GridPanel();
 		x = gridPanel.getX();
 		y = gridPanel.getY();
 		myBoard = new Board(gameNr, db_conn);
-
+		myStreet = new Street();
 	}
 
 	public void checkStreets() {
@@ -88,10 +90,39 @@ public class GridController {
 		}
 	}
 
-	public void showStreets() {
-		// pak alle mogelijke straten
-		// controleer soort straat
-		// teken een blok er om heen
+	public void showStreets(int player) {
+		ArrayList<Corner> streets = myStreet.getCornerPoints();
+		boolean yourStreet = (myStreet.getPlayer() == player);
+		if (x1 == x2) {
+			// a	(x1-5),y1
+			// b	(x1+5),y1
+			// c	(x2-5),y2
+			// d	(x2+5),y2
+		}
+		else {
+			double Dx;
+			double Dy;
+			int D;
+			// W is de breedte
+			int W = 10;
+
+			Dx = x2 - x1;
+			Dy = y2 - y1;
+			D = (int) Math.sqrt((Dx * Dx) + (Dy * Dy));
+			Dx = (((0.5 * W) * Dx) / D);
+			Dy = (((0.5 * W) * Dy) / D);
+
+			// dit zijn de hoekpunten
+			double aX = x1 - Dy;
+			double aY = y1 + Dx;
+			double bX = x1 + Dy;
+			double bY = x1 - Dy;
+			double cX = x2 - Dy;
+			double cY = y2 + Dx;
+			double dX = x2 + Dx;
+			double dY = y2 - Dx;
+		}
+
 		// g.setColor(kleur van speler)
 		// maak transparant
 		// g.fill
@@ -101,8 +132,6 @@ public class GridController {
 		// for(int i=0; i<arraysize; i++){
 		// x1 = x locatie eerste hoekpunt
 		// y1 = y locatie eerste hoekpunt
-		// x2 = x locatie tweede hoekpunt
-		// y2 = y locatie tweede hoekpunt
 		int r = 10;
 		int counter = 0;
 		int location;
@@ -120,19 +149,29 @@ public class GridController {
 		// }
 	}
 
-	public void showTowns() {
+	public void showTowns(int player) {
 		Corner myCorner = new Corner();
 		ArrayList<Corner> allPoints = myBoard.getCorners();
+		ArrayList<Corner> streets = myStreet.getCornerPoints();
 		ArrayList<Corner> possibleTowns = new ArrayList<Corner>();
 
 		for (int i = 0; i < allPoints.size(); i++) {
 			boolean hasTown = myCorner.isTown();
 			boolean hasCity = myCorner.isCity();
-			boolean connectedToYourStreet = false;
-			boolean noOtherTownsCitys = false;
+			boolean yourStreet = (myStreet.getPlayer() == player);
+			ArrayList<Point> neighbor = myCorner.getLinePoints();
+			boolean hasNeighbor = false;
+				for(int c = 0; c < 3; c++) {
+					Point possibleNeighbor = neighbor.get(c);
+					if(myCorner.isTown() || myCorner.isCity()) {
+						hasNeighbor = true;
+					}
+				}
+			
+			Boolean connectedToYourStreet = (yourStreet); 
 			if (!hasTown || !hasCity) {
 				if (connectedToYourStreet) {
-					if (noOtherTownsCitys) {
+					if (!hasNeighbor) {
 						possibleTowns.add(allPoints.get(i));
 					}
 
@@ -147,10 +186,11 @@ public class GridController {
 	}
 
 	public void showCities() {
-		Boolean yourTown = true;
+		Corner myCorner = new Corner();
 		ArrayList<Corner> allPoints = myBoard.getCorners();
 		ArrayList<Corner> yourTowns = new ArrayList<Corner>();
 		for (int i = 0; i < allPoints.size(); i++) {
+			Boolean yourTown = myCorner.isTown();
 			if (yourTown) {
 				yourTowns.add(allPoints.get(i));
 			}

3a09832e5bb7e8b06b46bceeb9c987f534253e37
Danish Afzal <d.afzal@hotmail.com>
2018-05-10 13:10:01 +0200

    Ik begin hier opnieuw aan
    Heb niks aan lege methods.

--- Project_Kolonisten_Van_Catan/src/View/EndPanel.java
+++ Project_Kolonisten_Van_Catan/src/View/EndPanel.java
@@ -15,55 +15,10 @@ import javax.swing.JPanel;
 import Controller.EndController;
 
 public class EndPanel extends JPanel {
-	private JButton repeat = new JButton();
-	private JButton end = new JButton();
 	private EndController endController;
-	private BufferedImage backgroundImage;
 
 	public EndPanel(EndController endController) {
 		this.endController = endController;
-		add(repeat);
-		add(end);
 
-		repeat.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				repeat();
-			}
-		});
-
-		end.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-//				end();
-			}
-		});
-		
-		
-		
 	}
-
-	public void SetVariables() {
-		// Add all images to Variables
-		try {
-			backgroundImage = ImageIO.read(new File("images/Backgroung/catan.jpg"));
-		} catch (IOException e) {
-			setBackground(new Color(59, 89, 182));
-		}
-	}
-
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		// g.drawImage(naam, 10, 200, (int) (naam.getWidth() * 0.2), (int)
-		// (naam.getHeight() * 0.2), this);
-	}
-	
-	public void repeat() {
-		// restart game met zelfde players
-	}
-
-//	public void end() {
-//		endController.endGame();
-//	}
 }
